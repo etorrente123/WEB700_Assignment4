25a63a6c77cc88991c05645f60b3f3920defab6b
eli <eli@gmail.com>
2023-07-20 23:37:24 -0400

    updates

--- server.js
+++ server.js
@@ -18,111 +18,144 @@ let path = require("path");
 let collegeData = require("./modules/collegeData");
 const { countReset } = require("console");
 eli.use(express.urlencoded({ extended: true}));
+const exphbs = require('express-handlebars');
+
+eli.use(express.urlencoded({extended: true}));
+
+eli.engine('hbs', exphbs.engine({ extname: '.hbs', defaultLayout: path.join(__dirname, 'views/layouts/main') }));
+eli.set('view engine', 'hbs');
 
 
-eli.get('/students/add', (req, res)=>{
-    res.sendFile(__dirname + '/views/addStudent.html');
-})
+eli.use(function(req, res, next) {
+    let route = req.path.substring(1);
+    eli.locals.activeRoute = "/" + (isNaN(route.split('/')[1]) ? route.replace(/\/(?!.*)/, "") : route.replace(/\/(.*)/, ""));
+    next();
+  });
 
-eli.post("/students/add", (req, res) => {
-    collegeData.addStudent(req.body)
-    .then(() => {
-        res.redirect("/student");
-    })
-    .catch((error)=>{
-        console.error(error);
-        res.redirect('404!' + error);
-    });
+const hbs = exphbs.create({
+    helpers: {
+      navLink: function(url, options) {
+        return '<li' + ((url == eli.locals.activeRoute) ? ' class="nav-item active" ' : ' class="nav-item" ') +
+               '><a class="nav-link" href="' + url + '">' + options.fn(this) + '</a></li>';
+      },
+
+      equal: function(lvalue, rvalue, options) {
+
+      }
+    }
 });
 
-
-eli.get("/", (req, res)=>{
-    res.sendFile(path.join(__dirname, "views","home.html"));
-});
-
-eli.get("/about", (req, res)=>{
-    res.sendFile(path.join(__dirname, "views","about.html"));
-});
-
-eli.get("/htmlDemo", (req, res)=>{
-    res.sendFile(path.join(__dirname, "views","htmlDemo.html"));
-});
-
-eli.get("/theme.css", (req, res) => {
-    res.sendFile(path.join(__dirname, "/css/theme.css"));
-});
-
-eli.get("/tas", (req, res)=>{
-    collegeData.getTAs()
-    .then((tas)=>{
-        if(tas.length == 0){
-            res.json({message: "error"});
-        }else{
-            res.json(tas);
-        }
-    })
-    .catch((err)=>{
-        res.status(500).json({error:err.message});
-    });
-});
-
-eli.get("/courses", (req,res)=>{
-    collegeData.getCourses()
-    .then((courses)=>{
-        if(courses.length===0){
-            res.json({message:"error"});
-        }else{
-            res.json(courses);
-        }
-    })
-    .catch((err)=>{
-        res.status(500).json({error:err.message});
-    });
-});
-
-eli.get("/student/:num", (req,res)=>{
-    var paramNum = req.params.num;
-    collegeData.getStudentByNum(paramNum)
-    .then((student)=>{
-        if(!student){
-            console.log("error");
-        }else{
-            res.json(student);
-        }
-    })
-    .catch((err)=>{
-        res.status(500).json({error: err.message});
-    });
-});
-
-eli.get("/student", (req,res)=>{
-    var courses = req.query.course;
-    collegeData.getAllStudents()
-    .then((students)=>{
-        if(students.length === 0){
-            console.log("no student")
-        }else{
-            if(courses){
-                return collegeData.getStudentsByCourse(courses);
-            }else{
-                return students;
-            }
-        }
-    })
-    .then((studentByCourse) => {
-        res.json(studentByCourse)
-    })
-    .catch((err)=>{
-        res.status(500).json({error: err.message});
-    });
-});
+  
+eli.engine('hbs', hbs.engine);  
 
 collegeData.initialize()
-    .then(()=>{
-        eli.listen(HTTP_PORT, () =>{
-            console.log("Server now on " + HTTP_PORT);
+    .then(() => {
+        eli.get('/', (req, res) => {
+            res.render('home');
+          });
+        
+        eli.get('/students', (req, res) => {
+            collegeData.getAllStudents()
+              .then((data) => {
+                res.render('students', { students: data });
+              })
+              .catch((err) => {
+                res.render('students', { message: "no results" });
+              });
+          });
+
+        eli.get('/students/add', (req, res) => {
+            res.render('addStudent');
+          });
+
+        eli.post("/students/add", (req, res)=>{
+            collegeData.addStudent(req.body)
+            .then(()=>{
+                res.redirect("/students")
+            })
+            .catch((error)=>{
+                console.error(error)
+                res.redirect('ERROR' + error);
+            })
+        })
+        eli.post("/students/update", (req, res) => {
+          collegeData
+            .updateStudent({ studentNum: parseInt(req.body.studentNum), ...req.body })
+            .then((updatedStudent) => {
+              res.redirect("/students");
+            })
+            .catch((error) => {
+              console.error(error);
+              res.redirect("/students");
+            });
+        });
+        
+        eli.get("/tas", (req, res) => {
+            collegeData.getTAs()
+                .then((tas) => {
+                    res.json(tas);
+                })
+                .catch((error) => {
+                    res.status(404).json({ message: "no results" });
+                });
+        });
+        
+        eli.get('/courses', (req, res) => {
+            collegeData.getCourses()
+              .then((data) => {
+                res.render('courses', { courses: data });
+              })
+              .catch((err) => {
+                res.render('courses', { message: "no results" });
+              });
+        });
+
+        eli.get('/courses/:id', (req, res) => {
+            const courseId = req.params.id;
+            collegeData.getCourseById(courseId)
+              .then((course) => {
+                res.render('course', { course });
+              })
+              .catch((err) => {
+                res.render('course', { message: "no results" });
+              });
+        });
+          
+        
+        eli.get('/students/:studentNum', (req, res) => {
+            const studentNum = parseInt(req.params.studentNum);
+        
+            collegeData.getStudentByNum(studentNum)
+                .then((student) => {
+                    res.render('student', { student });
+                })
+                .catch((err) => {
+                    res.render('student', { message: "no results" });
+                });
+        });
+
+        eli.get("/theme.css", (req,res) => {
+            res.sendFile(path.join(__dirname, "/css/theme.css"))
+        });
+        
+        
+        eli.get('/about', (req, res) => {
+            res.render('about');
+          });
+        
+        eli.get('/htmlDemo', (req, res) => {
+            res.render('htmlDemo');
+        });
+        
+        eli.use((req, res) => {
+            res.status(404).send("Page Not Found");
+        });
+
+
+        eli.listen(HTTP_PORT, () => {
+            console.log("Server listening on port: " + HTTP_PORT);
         });
     })
-    .catch((err)=>{
-        console.error(err)
+    .catch((error) => {
+        console.error(error);
     });
